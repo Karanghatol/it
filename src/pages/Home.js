40ecb34cb65b936feff0e303b2ad7024f525148a
karan keshav ghatol <122148804+Karanghatol@users.noreply.github.com>
2025-07-24 15:39:50 +0530

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { ArrowRight, BookOpen, Award, TrendingUp, Code, Database, Shield, Cpu } from 'lucide-react';
+import { ArrowRight, Users, BookOpen, Award, TrendingUp, Code, Database, Shield, Cpu } from 'lucide-react';
 import './Home.css';
 
 const Home = () => {
